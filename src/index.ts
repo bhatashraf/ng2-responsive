811d42ce00bab6f880bcb5da2368d3a35a02cd06
manucutillas <manucutillas@outlook.com>
2016-08-31 20:36:05 +0200

    Fixed - remove dev directives in npm package

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@
  * @Created_by Manu Cutillas
  * @Contributors Christophe HOARAU, Kamil Breguła
  * @created_at May 23, 2016
- * @updated_at JUN 26, 2016 - by ManuCutillas
- * @version_0.3.0
+ * @updated_at Aug 1, 2016 - by ManuCutillas
+ * @version_0.4.1
  *
  * Dependencies:
- * @angular/core : "2.0.0-rc.1"
+ * @angular/core : "2.0.0-rc.4"
  * rxjs: "5.0.0-beta.6"
  *
- * @more_info http://kalypso.agency
+ * @more_info https://kalypso.agency
  *            https://github.com/ManuCutillas
  *            https://www.npmjs.com/~manucutillas
  *            https://github.com/no-more
@@ -29,7 +29,6 @@ import {ShowItSizes, HideItSizes} from './custom-sizes/custom-sizes-directives';
 import {Is1xPixel, IsRetina, Is4k, PixelRatioInfo} from './pixelratio/pixelratio-directives';
 import {IsChrome, IsFirefox, IsSafari, IsOpera, IsIE, IsIE9, IsIE10, IsIE11, IsIE12, ShowItBrowser, HideItBrowser, ShowIEVersion, HideIEVersion, IeInfo, BrowserInfo} from './browsers/browsers-directives';
 import {Responsive} from './responsive/responsive';
-import {ResponsiveClass} from './responsive-css/responsive-css';
 import {ResponsiveWindow} from './responsive-window/responsive-window';
 import { UserAgentInfo } from './useragent/useragent';
 /* EXPORT => MODULES */
@@ -41,7 +40,6 @@ export * from './custom-sizes/custom-sizes-directives';
 export * from './pixelratio/pixelratio-directives';
 export * from './browsers/browsers-directives';
 export * from './responsive/responsive';
-export * from './responsive-css/responsive-css';
 export * from './responsive-window/responsive-window';
 export * from './useragent/useragent';
 /* RESPONSIVE DIRECTIVES */
@@ -49,4 +47,4 @@ export const RESPONSIVE_DIRECTIVES = [
     Responsive, XL, LG, MD, SM, XS, ShowItBootstrap, HideItBootstrap, IsSmartTv, IsDesktop, IsTablet, IsMobile, ShowItDevice, HideItDevice, IsIphone, IsIpad,
     IsAndroidMobile, IsAndroidTablet, IsWindowsPhone, ShowItStandard, HideItStandard, IsPortrait, IsLandscape, ShowItSizes, HideItSizes,
     Is1xPixel, IsRetina, Is4k, PixelRatioInfo, IsChrome, IsFirefox, IsSafari, IsOpera, IsIE, IsIE9, IsIE10, IsIE11, IsIE12, IeInfo, ShowItBrowser, HideItBrowser, ShowIEVersion, HideIEVersion,
-    ResponsiveSizeInfo, DeviceInfo, OrientationInfo, ResponsiveClass, ResponsiveWindow, UserAgentInfo ]; 
+    ResponsiveSizeInfo, DeviceInfo, OrientationInfo, ResponsiveWindow, UserAgentInfo ]; 
